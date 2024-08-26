af19d4670525ace156b9263486cd95165b5ffd90
Mac <mac@Macui-MacBookAir.local>
2024-08-26 15:26:45 +0900

    start

---
+++ src/main/java/com/example/petHotel/PetHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.petHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHotelApplication.class, args);
+	}
+
+}
